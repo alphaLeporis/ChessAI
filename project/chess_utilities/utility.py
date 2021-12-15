b459c99b86bfd8fb682e3d5b207d57addfeaedb6
Niels Van Gorp <vangorpniels@gmail.com>
2021-12-15 16:30:10 +0100

    Aangemaakt van alphabeta_agent.py

--- project/chess_utilities/utility.py
+++ project/chess_utilities/utility.py
@@ -7,3 +7,4 @@ class Utility(ABC):
     # Determine the value of the current board position (high is good for white, low is good for black, 0 is neutral)
     def board_value(self, board: chess.Board):
         pass
+
