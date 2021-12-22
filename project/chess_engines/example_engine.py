87028387a7680ac2880184e6d10c7c9aab186143
Niels Van Gorp <vangorpniels@gmail.com>
2021-12-22 16:44:44 +0100

    Engine aangepast niet meer de example

--- project/chess_engines/example_engine.py
+++ project/chess_engines/example_engine.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
 from project.chess_engines.uci_engine import UciEngine
 import chess
-from project.chess_agents.example_agent import ExampleAgent
-from project.chess_utilities.example_utility import ExampleUtility
+from project.chess_agents.AlphaBetaPruningNullMoveQueiscence import AlphaBetaPruningNullMoveQueiscence
+from project.chess_utilities.NewUtility import NewUtility
 
 if __name__ == "__main__":
     # Create your utility
-    utility = ExampleUtility()
+    utility = NewUtility()
     # Create your agent
-    agent = ExampleAgent(utility, 5.0)
+    agent = AlphaBetaPruningNullMoveQueiscence(utility, 5.0)
     # Create the engine
-    engine = UciEngine("Example engine", "Arne", agent)
+    engine = UciEngine("AlphaBetaPruningNullQuiscence engine", "Alexander", agent)
     # Run the engine (will loop until the game is done or exited)
     engine.engine_operation()
